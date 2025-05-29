1977d8153f794772260e93265b7692fb5361fdb5
isaias silvestri <isaiassilvestri20@gmail.com>
2025-05-29 14:05:35 -0300

    apartado administracion agregado, js, salones-crud

--- js/LocalStorage.js
+++
@@ -1,6 +0,0 @@
-export const initializeLocalStorage = () => {
-    if (!localStorage.getItem('salonesDeEventos')) {
-        localStorage.setItem('salonesDeEventos', JSON.stringify([])); 
-        console.log('LocalStorage de salones inicializado.');
-    }
-};
